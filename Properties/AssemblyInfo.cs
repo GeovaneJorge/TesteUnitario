0f5029433537cebf0990c201a8add85f312e277e
Geovane Jorge <geovane.jorge@gmail.com>
2022-05-31 11:15:57 -0300

    Versão 1.0

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestPradrao")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestPradrao")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6c295c9a-406d-431e-ae5b-09bb27e1c53b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Projeto.Shared;
+using System;
+
+namespace UnitTestPradrao
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestarIdadeMaior18()
+		{
+			int idade = 18;
+			bool resultado = Helper.VerificarIdadeRisco(idade);
+			Assert.IsTrue(resultado);
+		}
+
+		[TestMethod]
+		public void TestarNomeRisco()
+		{
+			string nome = "Geovane";
+			bool resultado = Helper.VerificarNomeDeRisco(nome);
+			Assert.IsTrue(resultado);
+		}
+	}
+}
